4fb2a1953ead0e5d0075265797d95fdb88f7f329
Mujahedyousef <mujahedyousef97@gmail.com>
2022-05-16 02:06:44 +0300

    complete code challenge 31

---
+++ javascript/code-challenges/data _structure/hash-Table/hash_map_repeted/hash_map.js
@@ -0,0 +1,19 @@
+"use strict";
+const hash_table=require('../hash_table/hash_table');
+
+function hash_Map_repeated_Word(str) {
+    let strArr = str.toLowerCase().split(" ");
+
+    const strHash = new hash_table(strArr.length);
+
+    for (let i = 0; i < strArr.length; i++) {
+        if (strHash.contain(strArr[i])) {
+            // console.log(strArr[i]);
+            return strArr[i];
+        }
+        strHash.set(strArr[i], strArr[i]);
+    }
+    return 'not found any repeated word';
+
+}
+module.exports=hash_Map_repeated_Word;
