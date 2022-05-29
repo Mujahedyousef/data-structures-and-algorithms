3a0c7e30ed5f6f5718f7c7f5a31541371feff227
Mujahedyousef <mujahedyousef97@gmail.com>
2022-05-29 03:27:06 +0300

    solve the graph

--- javascript/code-challenges/data _structure/graph/graph.js
+++ javascript/code-challenges/data _structure/graph/graph.js
@@ -19,7 +19,7 @@ class Graph{
   }
 
   addVertex(vertex){
-    this.list.set(vertex, []); // .set() is a Map method
+    this.list.set(vertex, []);
     return vertex;
   }
 
@@ -28,7 +28,33 @@ class Graph{
       return null;
     }
     let edge = new Edge(endVertex, weight);
-    let start = this.list.get(startVertex); // .get() is a Map method
+    let start = this.list.get(startVertex); 
     start.push(edge);
   }
+
+  getAll(){
+    let graph = [];
+    for(const [vertex, edge] of this.list.entries()){
+      graph.push(vertex, edge);
+    }
+    return graph;
+  }
+
+  getNeighbors(vertex){
+    let result = [];
+    if(!this.list.has(vertex)){
+      return null;
+    }
+    let vertexFromList = this.list.get(vertex);
+    for(let i=0; i<vertexFromList.length; i++){
+      result.push(vertexFromList[i].vertex);
+    }
+    return result;
+  }
+
+  size(){
+    return this.list.size; 
+  }
 }
+
+module.exports = { Vertex, Edge, Graph };
