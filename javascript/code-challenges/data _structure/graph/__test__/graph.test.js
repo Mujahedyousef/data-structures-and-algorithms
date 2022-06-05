8aaba28612d44f46e3c6d2f927d487a6e4b5bcad
Mujahedyousef <mujahedyousef97@gmail.com>
2022-06-05 15:54:18 +0300

    logic && test

--- javascript/code-challenges/data _structure/graph/__test__/graph.test.js
+++ javascript/code-challenges/data _structure/graph/__test__/graph.test.js
@@ -43,6 +43,41 @@ describe('testing the graph class', () => {
     myGraph.addEdge(vertexB,vertexG)
     expect(myGraph.breadthFirst(vertexA)).toEqual(["a","c","b","d","g","e","f"])
 })
+
+describe("Depth-first preOrder traversal on a graph", () => {
+  it("Depth First", () => {
+    let graph = new Graph();
+
+    let one = new Vertex("A");
+    let two = new Vertex("B");
+    let three = new Vertex("C");
+    let four = new Vertex("D");
+    let five = new Vertex("E");
+    let six = new Vertex("F");
+    let seven = new Vertex("G");
+    let eight = new Vertex("H");
+
+    graph.addVertex(one);
+    graph.addVertex(two);
+    graph.addVertex(three);
+    graph.addVertex(four);
+    graph.addVertex(five);
+    graph.addVertex(six);
+    graph.addVertex(seven);
+    graph.addVertex(eight);
+
+    graph.addEdge(one, two);
+    graph.addEdge(one, four);
+    graph.addEdge(two, three);
+    graph.addEdge(two, four);
+    graph.addEdge(three, seven);
+    graph.addEdge(four, five);
+    graph.addEdge(four, eight);
+    graph.addEdge(four, six);
+    graph.addEdge(six, eight);
+    expect(graph.depthFirstPreOrderTraversal(one)).toEqual("A,B,C,G,D,E,H,F,");
+  });
+});
 });
 
 
